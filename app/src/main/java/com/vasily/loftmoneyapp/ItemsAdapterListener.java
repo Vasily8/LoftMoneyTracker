0023eac25dbd040ff1070d89ebc4c957b525cf0a
Vasily <gurin.vasili@yandex.ru>
2018-09-14 15:59:38 +0300

    Action mode

---
+++ app/src/main/java/com/vasily/loftmoneyapp/ItemsAdapterListener.java
@@ -0,0 +1,10 @@
+package com.vasily.loftmoneyapp;
+
+
+public interface ItemsAdapterListener {
+    void onItemClick(Item item, int position);
+    void onItemLongClick(Item item, int position);
+
+
+
+}
